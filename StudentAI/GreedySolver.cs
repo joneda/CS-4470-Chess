5768bdac3f7ee3225d0ae837ab6dbc2c416e140c
David Jones <djones@dlapartments.com>
2013-09-26 01:51:27 -0600

    Added a basic greedy solver and heuristic

---
+++ StudentAI/GreedySolver.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UvsChess;
+
+namespace StudentAI
+{
+    internal class GreedySolver
+    {
+        private Func<ChessState, int> heuristic;
+        private Func<bool> timesUp;
+
+        public GreedySolver(Func<ChessState, int> heuristic, Func<bool> timesUp)
+        {
+            this.heuristic = heuristic;
+            this.timesUp = timesUp;
+        }
+
+        public ChessMove GetMove(ChessState startingState)
+        {
+            ChessState current = null;
+            Stack<ChessState> nodes = new Stack<ChessState>();
+            HashSet<string> visited = new HashSet<string>();
+
+            nodes.Push(startingState);
+
+            while (!timesUp() && nodes.Count > 0)
+            {
+                current = nodes.Pop();
+
+                if (visited.Contains(current.PartialFenBoard))
+                    continue;
+
+                visited.Add(current.PartialFenBoard);
+
+                List<ChessState> possible = new List<ChessState>();
+                foreach (ChessMove move in current.AllPossibleMoves)
+                {
+                    possible.Add(current.Move(move));
+                }
+
+                possible.Sort(delegate(ChessState x, ChessState y)
+                {
+                    if (x == null && y == null)
+                        return 0;
+
+                    if (x == null)
+                        return -1;
+
+                    if (y == null)
+                        return 1;
+
+                    return heuristic(x).CompareTo(heuristic(y));
+                });
+
+
+                foreach (ChessState state in possible)
+                {
+                    nodes.Push(state);
+                }
+            }
+
+            while (current.Depth > 1)
+                current = current.PreviousState;
+
+            return current.ActionPerformed;
+        }
+    }
+}
---
+++ StudentAI/Heuristics.cs
@@ -0,0 +1,36 @@
+﻿using UvsChess;
+
+namespace StudentAI
+{
+    internal static class Heuristics
+    {
+        public static int ValueOne(ChessState state)
+        {
+            int result = 0;
+            ChessPiece[,] rawBoard = state.RawBoard;
+
+            for (int x = 0; x < rawBoard.GetLength(0); x++)
+            {
+                for (int y = 0; y < rawBoard.GetLength(1); y++)
+                {
+                    ChessPiece piece = rawBoard[x,y];
+
+                    if (piece == ChessPiece.Empty)
+                        continue;
+
+                    if ((state.Color == ChessColor.Black && piece < ChessPiece.Empty)
+                        || (state.Color == ChessColor.White && piece > ChessPiece.Empty))
+                    {
+                        result++;
+                    }
+                    else
+                    {
+                        result--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
